cc2a2d46261f30c45fb9fd56d86a2441b2ecc982
torndeco <torndeco@localhost>
2016-12-20 12:47:03 +0000

    WIP Updating Build System to support 64bit builds

--- cmake_modules/FindMariaDB.cmake
+++ cmake_modules/FindMariaDB.cmake
@@ -5,12 +5,18 @@
 #  MYSQL_LIBRARIES   - List of libraries when using MYSQL.
 #  MYSQL_FOUND       - True if MYSQL found.
 
+if (CMAKE_CL_64)
+	SET(MYSQL_WINPATH "C:/Program Files/mariadb-connector-c")
+else()
+	SET(MYSQL_WINPATH "C:/Program Files (x86)/mariadb-connector-c")
+endif()
+
 FIND_PATH(MYSQL_INCLUDE_DIR NAMES mysql.h
   PATHS
   /usr/include/mariadb
   /usr/local/include/mariadb
   /usr/local/mariadb/include
-  "C:/Program Files (x86)/mariadb-connector-c/include/mariadb"
+  ${MYSQL_WINPATH}/include/mariadb
 )
 
 FIND_PATH(MYSQL_LIB_DIR NAMES mariadbclient.lib
@@ -19,13 +25,13 @@ FIND_PATH(MYSQL_LIB_DIR NAMES mariadbclient.lib
   /usr/lib/i386-linux-gnu
   /usr/local/lib
   /usr/local/mysql/lib
-  "C:/Program Files (x86)/mariadb-connector-c/lib/mariadb"
+	${MYSQL_WINPATH}/lib/mariadb
 )
 
 SET(MYSQL_NAMES mariadbclient)
 FIND_LIBRARY(MYSQL_LIBRARY
   NAMES ${MYSQL_NAMES}
-  PATHS /usr/lib /usr/local/lib /usr/local/mariadb/lib "C:/Program Files (x86)/mariadb-connector-c/lib"
+  PATHS /usr/lib /usr/local/lib /usr/local/mariadb/lib ${MYSQL_WINPATH}/lib
   PATH_SUFFIXES mariadb
 )
 
